b190921d1ec16ae51db4648d09ffa3a6d22b0df8
eightseconds <paulseokcho@gmail.com>
2019-10-17 23:01:47 -0700

    Implement solution

--- makeChange.js
+++ makeChange.js
@@ -1,3 +1,12 @@
 function numberOfWaysToMakeChange(n, denoms) {
-   
+    const ways = new Array(n + 1).fill(0);
+    ways[0] = 1
+    for (denom of denoms) {
+        for (let amount = 1; amount < ways.length; amount++) {
+            if (denom <= amount) {
+                ways[amount] += ways[amount - denom];
+            }
+        }
+    }
+    return ways[n]
 }
